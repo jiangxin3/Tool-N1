a1818860d997c3289b5d8b1a226bcb0ea110a89e
Shaokun Zhang <shaokunz@oci-nrt-cs-001-vscode-02.cm.cluster>
2025-05-14 17:43:59 -0700

    update license to apache

--- eval/bfcl_handler.py
+++ eval/bfcl_handler.py
@@ -1,8 +1,17 @@
-# Copyright (c) 2025, NVIDIA Corporation & Affiliates. All rights reserved. 
-# 
-# This work is made available under the Nvidia Source Code License-NC. 
-# To view a copy of this license, visit 
-# https://github.com/NVlabs/Tool-N1/blob/main/LICENSE
+# Copyright 2024 NVIDIA CORPORATION & AFFILIATES
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 from bfcl.model_handler.local_inference.base_oss_handler import OSSHandler
 from overrides import override
