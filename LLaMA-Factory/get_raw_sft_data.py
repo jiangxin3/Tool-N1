aef760b41171bde0d029f2a03a7f066b847c6965
Shaokun Zhang <shaokunz@oci-nrt-cs-001-vscode-02.cm.cluster>
2025-05-14 17:16:10 -0700

    add more license

--- LLaMA-Factory/get_raw_sft_data.py
+++ LLaMA-Factory/get_raw_sft_data.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2025, NVIDIA Corporation & Affiliates. All rights reserved. 
+# 
+# This work is made available under the Nvidia Source Code License-NC. 
+# To view a copy of this license, visit 
+# https://github.com/NVlabs/Tool-N1/blob/main/LICENSE
+
 import json
 import os
 
